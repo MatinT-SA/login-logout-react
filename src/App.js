540af48dfc0a4d83aca0a9a68e069aa5633524f2
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2023-04-23 23:36:40 +0430

    changing useContext for logging out instead of passing the logout handler through props

--- src/App.js
+++ src/App.js
@@ -29,17 +29,16 @@ function App() {
     };
 
     return (
-        <React.Fragment>
-            <AuthContext.Provider value={{
-                isLoggedIn: isLoggedIn,
-            }}>
-                <MainHeader onLogout={logoutHandler} />
-                <main>
-                    {!isLoggedIn && <Login onLogin={loginHandler} />}
-                    {isLoggedIn && <Home onLogout={logoutHandler} />}
-                </main>
-            </AuthContext.Provider>
-        </React.Fragment>
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+        }}>
+            <MainHeader />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </AuthContext.Provider>
     );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import classes from './Navigation.module.css';
 import AuthContext from '../../context/auth-content';
 
-const Navigation = (props) => {
+const Navigation = () => {
     const ctx = useContext(AuthContext);
 
     return (
@@ -21,7 +21,7 @@ const Navigation = (props) => {
                 )}
                 {ctx.isLoggedIn && (
                     <li>
-                        <button onClick={props.onLogout}>Logout</button>
+                        <button onClick={ctx.onLogout}>Logout</button>
                     </li>
                 )}
             </ul>
